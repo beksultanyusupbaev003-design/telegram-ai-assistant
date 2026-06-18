00cd2a015cc78491889ae4ac370675c1700e3239
beksultanyusupbaev003-design <beksultanyusupbaev003@gmail.com>
2026-06-19 01:52:51 +0500

    Load environment variables for bot configuration
    Added environment variable loading for bot token and API key.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("Telegram AI Assistant ishga tushdi!")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8859863032:AAG4pj0ybaGJdblqiIc_DF6rtOLiLpcrvV4")
+GEMINI_API_KEY = os.getenv("AQ.Ab8RN6IKYgL7Z5U9G-p-Wh2yVZSUqrX0uELosmkKYoZvuTZnjQ")
+
+print("===================================")
+print("Telegram AI Assistant")
+print("Bot Token:", BOT_TOKEN)
+print("Gemini API:", GEMINI_API_KEY)
+print("Bot ishga tushdi!")
+print("===================================")
